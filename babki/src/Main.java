99c728b483ee375a4959d25bc745210cb289abee
Damir Koblev <101816707+TechnoDamo@users.noreply.github.com>
2023-10-09 13:39:27 +0300

    Update Main.java

--- babki/src/Main.java
+++ babki/src/Main.java
@@ -4,15 +4,11 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        for(int i = 0; i < 120; i++)
-        {
-            for(int j = 0; j < 100; j++)
-            {
-                money m = new money(i, j);
-                m.print();
-                System.out.println();
-            }
-        }
+        money m = new money(10, 10);
+        m.print();
+        System.out.println();
+        m.add(100, 1000);
+        m.print();
 
         }
     }
