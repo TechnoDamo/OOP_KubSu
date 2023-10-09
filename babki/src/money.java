b20de2cedbe9f91ef5d9bb062a785d0c82602d31
Damir Koblev <101816707+TechnoDamo@users.noreply.github.com>
2023-10-09 13:40:19 +0300

    Update money.java

--- babki/src/money.java
+++ babki/src/money.java
@@ -1,19 +1,51 @@
 public class money {
     int rub;
     int cop;
+    int smR = 0, smCop = 0;
     money()
     {
         this.rub = 0;
         this.cop = 0;
+        smR += rub;
+        smCop = cop;
     }
     money(int rub)
     {
         this.rub = rub;
+        this.cop = 0;
+        smR += rub;
+        smCop = cop;
     }
     money(int rub, int cop)
     {
         this.rub = rub+cop/100;
         this.cop = cop%100;
+        smR += rub;
+        smCop = cop;
+    }
+    void  add(int rub)
+    {
+        this.rub+=rub;
+    }
+    void  add(int rub, int cop)
+    {
+        this.rub+=rub+cop/100;
+        this.cop+=cop%100;
+    }
+    int getRub() {
+        return rub;
+    }
+
+    void setRub(int rub) {
+        this.rub = rub;
+    }
+
+    int getCop() {
+        return cop;
+    }
+
+    void setCop(int cop) {
+        this.cop = cop;
     }
     void print()
     {
@@ -33,21 +65,7 @@ public class money {
         if(kc == 1){c = " копейка ";}
         else if(kc == 2){c = " копейки ";}
         else{c = " копеек ";}
-        System.out.print("У вас " + rub + r + cop + c);
-    }
-    int getRub() {
-        return rub;
-    }
-
-    void setRub(int rub) {
-        this.rub = rub;
-    }
-
-    int getCop() {
-        return cop;
-    }
-
-    void setCop(int cop) {
-        this.cop = cop;
+        System.out.print("Итого у вас: " + rub + r + cop + c);
+        System.out.println();
     }
 }
