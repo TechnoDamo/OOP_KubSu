87717cce0514749db300d33ebf6d314512f41ac6
Damir Koblev <101816707+TechnoDamo@users.noreply.github.com>
2023-11-09 13:34:08 +0300

    Update Data.java

--- lab10_exceptions/Data.java
+++ lab10_exceptions/Data.java
@@ -6,46 +6,61 @@ public class Data {
         this.m = m;
         this.y = y;
     }
-    // output method
+    // вывод
     void print() {
-        System.out.println("Year: " + y + " " + "month: " + m + "day: " + y);
+        System.out.println("Year: " + y + " " + "month: " + m + " day: " + d);
         System.out.println();
     }
-    // method - add some days to the current date
-    void addDays(int extraDays) throws MyException{
+    // увеличить текущую дату на опр. кол-во дней
+    void addDays(int extraDays) {
         int extraYears = extraY(y, extraDays);
-        int k = 0;
+        int k = 0; //количество весокосных лет в данном промежутке
         for (int i = y; i <= extraYears + y; i++) {
             if(i % 4 == 0){ k++; }
         }
-        if(extraDays / (extraYears * 365 + k) != extraYears){ extraYears--; }
+        if(extraYears != 0 && extraDays / (extraYears * 365 + k) != 1){ extraYears--; }
         y += extraYears;
+        extraDays -= (extraYears * 365 + k);
 
-        int extraMonths;
-        extraDays -= extraYears * 365;
-        d += extraDays;
-        if (d > 31 || m > 12 || y < 0 || d < 0 || m < 0) throw new MyException("Incorrect date", d, m, y);
+        m = newM(m, extraDays);// в newM поменяли количество extraDays
     }
-    // count day number from the beginning of the year
 
+    // посчитать номер дня с начала года
+    int dayNum() {
+        int []months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if(y % 4 == 0) { months[1] = 29;}
+        int k = 0;
+        for(int i = 0; i < m; i++) {
+            k += months[i];
+        }
+        k += d;
+        return k;
+    }
+
+    int dayNum(int year, int month, int day) throws MyException{
+        if (d > 31 || m > 12 || y < 0 || d < 0 || m < 0) throw new MyException("Incorrect date", d, m, y);
+        int []months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if(year % 4 == 0) { months[1] = 29;}
+        int k = 0;
+        for(int i = 0; i < month; i++) {
+            k += months[i];
+        }
+        k += d;
+        return k;
+    }
 
     //вспомогательные методы
     int extraY(int Year, int extraDays) {
         int extraYears = extraDays / 365;
-        int k = 0;
-        for (int i = Year; i <= extraYears + Year; i++) {
-            if(i % 4 == 0){ k++; }
-        }
-        if(extraDays / (extraYears * 365 + k) != extraYears){ extraYears--; }
         return extraYears;
     }
-    int newM(int Month, int extraDays)
-    {
+    int newM(int Month, int extraDays) {
         int []months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         if(y % 4 == 0) { months[1] = 29;}
         while (extraDays >= months[Month]) {
             extraDays -= months[Month];
             Month++;
+            if(Month == 12){Month = 0; y++;}
         }
         d = extraDays;
         return Month;
